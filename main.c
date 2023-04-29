4918247657c53dbcc4cd6395a16291bcf7178f71
igor02031992 <aessakura@gmail.com>
2023-04-29 14:41:16 +0300

    Wrong change month count
    Signed-off-by: igor02031992 <aessakura@gmail.com>

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main()
     char *locale = setlocale(LC_ALL, "");
     int q,n,F,year = 0;
     float qnow;
+    int days = 31;
     printf("check commit");
     printf("Введите количество населения q = ");
     scanf("%d", &q);
@@ -18,16 +19,22 @@ int main()
     if(q>1000000) printf("Вы ввели неправильно количество населения");
     if(F>100) printf("Вы ввели неправильно прирост населения");
     if(n>10) printf("Вы ввели неправильно во сколько раз увеличится");
-printf("Население Прирост Увлечение", q, F, n);
-
+    printf("Население Прирост Увлечение", q, F, n);
     qnow = q;
-  
-			
     while(qnow<=q*n)
     {
         qnow = qnow+(qnow*1/F);
         year++;
         printf("\nГод: %d Рост населения: %.0f", year, qnow);
     }
+    if(month == "july")
+    {
+        days = 31;
+    }
+    if(month =="August"){
+        days = 31;
+    }
+    printf("\nthe day in month is %d\n" days);
+
     printf("\nЧерез %d лет в %d раз увеличится население", year, n);
 }
