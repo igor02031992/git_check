67c5bfd57cd622a39b4f55c1305c13ac0507f43c
igor02031992 <aessakura@gmail.com>
2023-04-29 12:48:35 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*библиотеки*/
+#include <math.h>
+#include <locale.h>
+int main()
+{
+    char *locale = setlocale(LC_ALL, "");
+    int q,n,F,year = 0;
+    float qnow;
+    printf("check commit");
+    printf("Введите количество населения q = ");
+    scanf("%d", &q);
+    printf("Введите прирост населения F = ");
+    scanf("%d", &F);
+    printf("Введите во сколько раз увеличится n = ");
+    scanf("%d", &n);
+    if(q>1000000) printf("Вы ввели неправильно количество населения");
+    if(F>100) printf("Вы ввели неправильно прирост населения");
+    if(n>10) printf("Вы ввели неправильно во сколько раз увеличится");
+    qnow = q;
+  printf("Население Прирост Увлечение", q, F, n);
+			q = 10+q;
+     printf("Население Прирост Увлечение", q, F, n);
+    printf("Население Прирост Увлечение", q, F, n);
+
+    while(qnow<=q*n)
+    {
+        qnow = qnow+(qnow*1/F);
+        year++;
+        printf("\nГод: %d Рост населения: %.0f", year, qnow);
+    }
+    printf("\nЧерез %d лет в %d раз увеличится население", year, n);
+}
