cfb9eff4fed3e5736eab621a47ed343470d8dd30
igor02031992 <aessakura@gmail.com>
2023-04-30 18:17:27 +0300

    Now you can only set number, for month, and get name month and days count

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 /*библиотеки*/
 #include <math.h>
 #include <locale.h>
+#include "month.h"
+#include "month.c"
 int main()
 {
     char *locale = setlocale(LC_ALL, "");
     int q,n,F,year = 0;
     float qnow;
+    int name_month;
+    int monthSelect,days;
 
-    int days = 31;
-
-    printf("check commit");
+    printf("\ncheck commit\n");
     printf("Введите количество населения q = ");
     scanf("%d", &q);
     printf("Введите прирост населения F = ");
@@ -23,18 +25,20 @@ int main()
     if(n>10) printf("Вы ввели неправильно во сколько раз увеличится");
     printf("Население Прирост Увлечение", q, F, n);
     qnow = q;
+    printf("\nВведите номер месяца в котором хотите посчитать количество дней: ", monthSelect);
+    scanf("%d", &monthSelect);
+    name_month = month (monthSelect,"NULL",&days);
+
     while(qnow<=q*n)
     {
         qnow = qnow+(qnow*1/F);
         year++;
         printf("\nГод: %d Рост населения: %.0f", year, qnow);
     }
-  
-     
-    if(month =="August"){
-        days = 31;
-    }
-    printf("\nthe day in month is %d\n" days);
+
+
+    printf("\n\nCurrent Month: %s", name_month);
+    printf("\nthe day in month is %d\n", days);
 
 
     printf("\nЧерез %d лет в %d раз увеличится население", year, n);
---
+++ month.h
@@ -0,0 +1,6 @@
+#ifndef MONTH_H_INCLUDED
+#define MONTH_H_INCLUDED
+
+extern int month (int monthSelect,char name_month[],int *days);
+
+#endif // MONTH_H_INCLUDED
